83599091b3bc9227862733bfbd4ef866c3da4fb5
siddharthdayalwal <siddharthdayalwal2000@gmail.com>
2020-09-07 16:38:13 +0530

    Contact Added

--- Latest One/src/script.js
+++
@@ -1,9 +0,0 @@
-$(".prod-logo").hover(function () {
-    $(".inner").addClass("inner-hover");
-    $(".nav-li").slideDown(500);
-  },
-  function () {
-    $(".inner").removeClass("inner-hover");
-   $(".nav-li").slideUp(500);
-  }
-);
